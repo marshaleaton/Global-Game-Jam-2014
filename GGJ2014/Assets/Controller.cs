71aeb145046a70943a6e2c1f6f30a3cc00813a2c
Marshal <marshaleaton@gmail.com>
2014-01-25 09:48:15 -0500

    Working on level layout

--- GGJ2014/Assets/Controller.cs
+++ GGJ2014/Assets/Controller.cs
@@ -2,9 +2,9 @@
 using System.Collections;
 
 public class Controller : MonoBehaviour {
-	public float speed = 6.0F;
-	public float jumpSpeed = 8.0F;
-	public float gravity = 20.0F;
+	public float speed = 0.5F;
+	public float jumpSpeed = .50F;
+	public float gravity = 10.0F;
 	private Vector3 moveDirection = Vector3.zero;
 	private Transform groundCheck;
 	private bool grounded = false;
@@ -16,6 +16,32 @@ public class Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update() {
 		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		//on demand output diagnostics to log
+		if (Input.GetKeyUp("l")) {
+			Debug.Log ("Is Grounded: "+grounded);
+		}
+
+		if (Input.GetKeyUp("a")) {
+			Debug.Log ("Is Grounded: "+grounded);
+		}
+		if (Input.GetKeyUp("w")) {
+
+			if(grounded){
+				moveDirection.y = jumpSpeed;
+			}
+		}
+
+		if(Input.GetKeyUp("space")){
+			SpreadSunshine ();
+		}
+		if (!grounded) {
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+		gameObject.transform.Translate (moveDirection);
 		/*CharacterController controller = GetComponent<CharacterController>();
 		if (controller.isGrounded) {
 			moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -27,6 +53,10 @@ public class Controller : MonoBehaviour {
 		}
 		moveDirection.y -= gravity * Time.deltaTime;
 		controller.Move(moveDirection * Time.deltaTime);*/
+	}
+
+	void SpreadSunshine(){
+		Debug.Log ("Fire");
 	}
 
 	//Basic collision detection checking for two differently named objects
